ac558318b4dbf2f25904c4587dca5b34dd66e599
52108 <52108@example.com>
2026-08-18 15:30:10 +0800

    fix Kindle layout rendering

--- docs/accounts-data.js
+++ docs/accounts-data.js
@@ -1,6 +1,6 @@
 /* generated locally; do not add API keys */
 window.ACCOUNTS_DATA = {
-  "updatedAt": "2026-08-18T07:15:10.838Z",
+  "updatedAt": "2026-08-18T07:30:10.166Z",
   "total": 9,
   "items": [
     {
@@ -22,11 +22,11 @@ window.ACCOUNTS_DATA = {
       "group": "客服",
       "status": "active",
       "schedulable": true,
-      "todayRequests": 7449,
-      "weekRequests": 11654,
+      "todayRequests": 7989,
+      "weekRequests": 12194,
       "fiveHour": 0,
-      "weekly": 34,
-      "fiveHourResetAt": "2026-08-18T15:15:10+08:00",
+      "weekly": 35,
+      "fiveHourResetAt": "2026-08-18T15:30:10+08:00",
       "weeklyResetAt": "2026-08-24T16:33:37+08:00"
     },
     {
@@ -35,11 +35,11 @@ window.ACCOUNTS_DATA = {
       "group": "森哥专属",
       "status": "active",
       "schedulable": true,
-      "todayRequests": 4348,
-      "weekRequests": 23389,
+      "todayRequests": 4550,
+      "weekRequests": 23591,
       "fiveHour": 0,
-      "weekly": 86,
-      "fiveHourResetAt": "2026-08-18T15:15:09+08:00",
+      "weekly": 87,
+      "fiveHourResetAt": "2026-08-18T15:30:08+08:00",
       "weeklyResetAt": "2026-08-20T11:30:19+08:00"
     },
     {
@@ -65,8 +65,8 @@ window.ACCOUNTS_DATA = {
       "weekRequests": 6379,
       "fiveHour": 0,
       "weekly": 100,
-      "fiveHourResetAt": "2026-08-18T15:15:10+08:00",
-      "weeklyResetAt": "2026-08-20T17:09:27+08:00"
+      "fiveHourResetAt": "2026-08-18T15:30:09+08:00",
+      "weeklyResetAt": "2026-08-20T17:09:26+08:00"
     },
     {
       "id": 32,
@@ -78,7 +78,7 @@ window.ACCOUNTS_DATA = {
       "weekRequests": 5258,
       "fiveHour": 0,
       "weekly": 99,
-      "fiveHourResetAt": "2026-08-18T15:15:10+08:00",
+      "fiveHourResetAt": "2026-08-18T15:30:09+08:00",
       "weeklyResetAt": "2026-08-20T17:20:56+08:00"
     },
     {
@@ -91,7 +91,7 @@ window.ACCOUNTS_DATA = {
       "weekRequests": 2873,
       "fiveHour": 0,
       "weekly": 100,
-      "fiveHourResetAt": "2026-08-18T15:15:08+08:00",
+      "fiveHourResetAt": "2026-08-18T15:30:08+08:00",
       "weeklyResetAt": "2026-08-20T11:33:45+08:00"
     },
     {
@@ -104,7 +104,7 @@ window.ACCOUNTS_DATA = {
       "weekRequests": 2166,
       "fiveHour": 0,
       "weekly": 100,
-      "fiveHourResetAt": "2026-08-18T15:15:09+08:00",
+      "fiveHourResetAt": "2026-08-18T15:30:08+08:00",
       "weeklyResetAt": "2026-08-20T11:30:55+08:00"
     },
     {
@@ -117,7 +117,7 @@ window.ACCOUNTS_DATA = {
       "weekRequests": 2319,
       "fiveHour": 0,
       "weekly": 100,
-      "fiveHourResetAt": "2026-08-18T15:15:08+08:00",
+      "fiveHourResetAt": "2026-08-18T15:30:08+08:00",
       "weeklyResetAt": "2026-08-20T11:31:36+08:00"
     }
   ]
